9c218562bb53d0fe77e234abc0967c90b1d15637
Dale Campbell <oshuma@gmail.com>
2009-08-16 08:44:21 -0500

    renaming some things

--- lib/mailinator.rb
+++ lib/mailinator.rb
@@ -1,13 +1,24 @@
-require 'uri'
+dependencies = %w{
+  nokogiri
+  open-uri
+  uri
+}
+
+begin
+  dependencies.each { |lib| require lib }
+rescue LoadError
+  require 'rubygems'
+  dependencies.each { |lib| require lib }
+end
 
 class Mailinator
   BASE_URL = 'http://mailinator.com'
 
-  attr_accessor :email, :primary
+  attr_accessor :email, :name
 
-  def initialize(primary = nil, opts = {})
-    @primary = primary
-    @email = format_email(primary)
+  def initialize(name = nil, opts = {})
+    @name = name
+    @email = format_email(name)
   end
 
   def inbox_url
@@ -19,13 +30,13 @@ class Mailinator
   end
 
   def widget_url(width = '250', height = '250')
-    URI.parse("#{BASE_URL}/widget/mailin8r.jsp?w=#{width}&h=#{height}&b=#{@primary}").to_s
+    URI.parse("#{BASE_URL}/widget/mailin8r.jsp?w=#{width}&h=#{height}&b=#{@name}").to_s
   end
 
   private
 
   def action_url(action)
-    URI.parse("#{BASE_URL}/#{action}.jsp?email=#{@primary}").to_s
+    URI.parse("#{BASE_URL}/#{action}.jsp?email=#{@name}").to_s
   end
 
   def format_email(email)

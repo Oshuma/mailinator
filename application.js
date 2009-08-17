c8d00fef3b7b88ff0e7c5d021f7564a641ebc209
Dale Campbell <oshuma@gmail.com>
2009-08-16 19:53:38 -0500

    adding demo

--- application.js
+++ application.js
@@ -16,10 +16,22 @@ $(document).ready(function() {
                  .css('-moz-border-radius-bottomleft', '0')
                  .css('-webkit-border-bottom-right-radius', '0')
                  .css('-webkit-border-bottom-left-radius', '0');
+
+    // Create an email from the input field.
+    var mail = new Mailinator($('#example-name').val());
+
+    $('#inbox-url').attr('target', '_blank')
+                   .attr('href', mail.inboxURL)
+                   .html(mail.inboxURL);
+    $('#rss-url').attr('target', '_blank')
+                 .attr('href', mail.rssURL)
+                 .html(mail.rssURL);
+
     $('#example-output').slideDown();
     return false;
   });
 
+  // Slide up and hide the example output before the default action.
   $('#example-form button[type="reset"]').click(function() {
     $('#example-output').slideUp(function() {
       $('#example').css('border-bottom', '3px solid #039')
